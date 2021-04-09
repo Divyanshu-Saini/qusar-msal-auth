e2b4c6eda17a2b7d4382020a5ae71be8c2e5ea73
Divyanshu <divyanshu.saini@ndsglobal.com>
2021-04-09 10:01:38 +0530

    clean up code

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -1,12 +1,3 @@
-// import msalPlugin from "vue-msal-browser";
-
-// "async" is optional;
-// more info on params: https://quasar.dev/quasar-cli/boot-files
-// export default async ({ Vue, store }) => {
-//   Vue.use(msalPlugin, store.state.authModule.msalConfig);
-// };
-
-
 import msalPlugin from "vue-msal-browser";
 
 export default ({ Vue, store }) => {
